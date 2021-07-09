df74934ac0908d0a5a980f4830c6353c45fca25b
c4ar <86037186+c4ar@users.noreply.github.com>
2021-07-08 22:21:43 -0700

    Add files via upload

---
+++ SURVEY PROJECT/js/Survey.js
@@ -0,0 +1,14 @@
+transition.js
+bootstrap.js
+
+$('#myModal').modal({
+  keyboard: false
+})// JavaScript source code
+
+
+$('#navbar').scrollspy()
+
+$('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+});
+
